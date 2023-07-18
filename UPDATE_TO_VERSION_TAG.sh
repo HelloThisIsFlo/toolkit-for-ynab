898ce4a4a3e3dc4292caf888de2e015cde44268d
Flo Kempenich <flo@kempenich.dev>
2023-07-18 13:46:34 +0100

    Add update instructions

--- UPDATE_TO_VERSION_TAG.sh
+++ UPDATE_TO_VERSION_TAG.sh
@@ -17,5 +17,12 @@ tag=$1
 git fetch --tags upstream "$tag"
 git merge $tag --strategy-option theirs
 
+echo "Next Steps"
+echo "1. Run 'yarn watch'"
+echo "2. Reload the extension: vivaldi://extensions/ (click link, it will be blank, reload page, it will work)"
+echo "3. Make sure the version is updated"
+echo "4. Make sure the extension works"
+echo "5. Force push the changes: 'gpf'"
+
 
 cd - >/dev/null
